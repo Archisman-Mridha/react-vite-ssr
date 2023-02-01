47fa870ce0763b725df3dbd92208629d9b8ea291
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-02-01 21:47:37 +0530

    checking css is working or not

--- apps/domigo-static/src/App.jsx
+++ apps/domigo-static/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/home.page"
 import { About } from "./pages/about.page"
 
+import "./styles/main.css"
+
 export function App( ) {
     return (
         <Routes>
